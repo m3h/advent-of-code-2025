9575aede9234cd1492dfa8aa9b3d73b0bc2685ed
Reece Volker <reecelyndon.volker@asml.com>
2025-12-01 11:14:59 +0100

    day 1 part 1, day 1 part 2 complete

---
+++ day1part1.el
@@ -0,0 +1,30 @@
+(setq remaining_moves (split-string
+(with-temp-buffer
+  (insert-file-contents "input_day1.txt")
+  (buffer-string)
+  )))
+
+(setq current_position 50)
+(setq count_of_zero 0)
+
+(while remaining_moves
+
+  (setq current_move (car remaining_moves))
+  (print current_move)
+  (setq current_direction (substring current_move 0 1))
+  
+  (setq current_rotations (string-to-number (substring current_move 1)))
+
+  (when (string-equal current_direction "L") (setq current_rotations (* current_rotations -1)))
+
+  (setq current_position (+ current_position current_rotations))
+  (setq current_position (% current_position 100))
+
+  (when (= current_position 0) (setq count_of_zero (+ count_of_zero 1)))
+    
+  (setq remaining_moves (cdr remaining_moves))
+  (print current_position)
+  (print count_of_zero)
+  )
+
+(print count_of_zero)
